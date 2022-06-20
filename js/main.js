a41191d216b7e062f30446899adcd37d2c9b3cfc
Vinh Le <vinhyuki1995@gmail.com>
2022-06-20 13:13:30 +0700

    add-header

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector(".navbar");
+const arrItemNavbar = ["Home","Education","Experience","Project","Contact Me"];
+for(let i=0; i<arrItemNavbar.length; i=i+1){
+  let navbarItemElement = document.createElement("li");
+  navbarItemElement.classList.add("navbar__item");
+
+  let navbarItemLink=document.createElement("div");
+  navbarItemLink.classList.add("navbar__item-link");
+  navbarItemLink.innerHTML=arrItemNavbar[i];
+
+  navbarItemElement.appendChild(navbarItemLink);
+   navbar.appendChild(navbarItemElement);
+
+ }
+
+ const bodyLeft = document.querySelector(".body__left");
+ let inforContent = document.createElement("div");
+ inforContent.classList.add("information__content");
+ bodyLeft.appendChild(inforContent);
+
+ const arrInformation = ["Hi","I'M  VINH","Deverloper - Fontend"];
+ for(let i=0; i<arrInformation.length; i=i+1){
+  let textElement = document.createElement("div");
+  textElement.classList.add("text");
+
+  textElement.innerHTML = arrInformation[i];
+  inforContent.appendChild(textElement);
+ }
