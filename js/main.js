1f0de135b22a91b55e03a3ce0e29b6d974b1ff5d
Vinh Le <vinhyuki1995@gmail.com>
2022-06-25 23:28:38 +0700

    fix-header

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 const navbar = document.querySelector(".navbar");
-const arrItemNavbar = ["Home","Education","Experience","Project","Contact Me"];
-for(let i=0; i<arrItemNavbar.length; i=i+1){
+const arrItemNavbar = [
+  "Home",
+  "Education",
+  "Experience",
+  "Project",
+  "Contact Me",
+];
+for (let i = 0; i < arrItemNavbar.length; i = i + 1) {
   let navbarItemElement = document.createElement("li");
   navbarItemElement.classList.add("navbar__item");
 
-  let navbarItemLink=document.createElement("div");
+  let navbarItemLink = document.createElement("div");
   navbarItemLink.classList.add("navbar__item-link");
-  navbarItemLink.innerHTML=arrItemNavbar[i];
+  navbarItemLink.innerHTML = arrItemNavbar[i];
 
   navbarItemElement.appendChild(navbarItemLink);
-   navbar.appendChild(navbarItemElement);
+  navbar.appendChild(navbarItemElement);
+}
 
- }
+const bodyLeft = document.querySelector(".body__left");
+let inforContent = document.createElement("div");
+inforContent.classList.add("information__content");
+bodyLeft.appendChild(inforContent);
 
- const bodyLeft = document.querySelector(".body__left");
- let inforContent = document.createElement("div");
- inforContent.classList.add("information__content");
- bodyLeft.appendChild(inforContent);
-
- const arrInformation = ["Hi","I'M  VINH","Deverloper - Fontend"];
- for(let i=0; i<arrInformation.length; i=i+1){
+const arrInformation = [
+  "Hi",
+  "I'M  VINH,",
+  "Web Deverloper",
+  "Deverloper - Fontend",
+];
+for (let i = 0; i < arrInformation.length; i = i + 1) {
   let textElement = document.createElement("div");
   textElement.classList.add("text");
 
   textElement.innerHTML = arrInformation[i];
   inforContent.appendChild(textElement);
- }
+}
